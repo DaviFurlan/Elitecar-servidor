af450b61560485fefa125395d4aff99528a848a2
davi.furlan@aluno.senai.br.com <davifurlan07@gmail.com>
2024-10-22 14:00:53 -0300

    trabalhando projeto

--- src/model/Cliente.ts
+++ src/model/Cliente.ts
@@ -1,56 +1,65 @@
+import { query } from "express";
+import { DatabaseModel } from "./DatabaseModel";
+
+const database = new DatabaseModel().pool;
+
 /**
- * Classe que representa o cliente.
+ * Representa um cliente com identificador único, nome, CPF e telefone.
  */
 export class Cliente {
 
-    /* Atributos */
-    /* Identificador do cliente */
+    /**
+     * Identificador único do cliente.
+     */
     private idCliente: number = 0;
-    /* nome do cliente */
+    /**
+     * Nome do cliente.
+     */
     private nome: string;
-    /* cpf do cliente */
+    /**
+     * CPF do cliente.
+     */
     private cpf: string;
-    /* telefone do cliente */
+    /**
+     * Telefone do cliente.
+     */
     private telefone: string;
 
     /**
-     * Construtor da classe Cliente
+     * Cria uma instância da classe Cliente.
      * 
-     * @param nome Nome do cliente
-     * @param cpf CPF do cliente
-     * @param telefone Telefone do cliente
+     * @param nome - O nome do cliente.
+     * @param cpf - O CPF do cliente.
+     * @param telefone - O telefone do cliente.
      */
-    constructor(
-        nome: string,
-        cpf: string,
-        telefone: string,
-    ) {
+    constructor(nome: string, cpf: string, telefone: string) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
     }
 
-    /* Métodos get e set */
     /**
-     * Recupera o identificador do cliente
-     * @returns o identificador do cliente
+     * Retorna o identificador único do cliente.
+     *
+     * @returns O identificador único do cliente.
      */
     public getIdCliente(): number {
         return this.idCliente;
     }
 
     /**
-     * Atribui um valor ao identificador do cliente
-     * @param idCliente novo identificado do cliente
+     * Define o identificador do cliente.
+     *
+     * @param idCliente - O novo identificador do cliente.
      */
     public setIdCliente(idCliente: number): void {
         this.idCliente = idCliente;
     }
 
     /**
-     * Retorna o nome do cliente.
+     * Obtém o nome do cliente.
      *
-     * @returns {string} O nome do cliente.
+     * @returns O nome do cliente.
      */
     public getNome(): string {
         return this.nome;
@@ -59,45 +68,130 @@ export class Cliente {
     /**
      * Define o nome do cliente.
      * 
-     * @param nome - Nome do cliente a ser definido.
+     * @param nome - O nome a ser definido para o cliente.
      */
     public setNome(nome: string): void {
         this.nome = nome;
     }
 
     /**
-     * Retorna o CPF do cliente.
+     * Obtém o CPF do cliente.
      *
-     * @returns {string} O cpf do cliente.
+     * @returns {string} O CPF do cliente.
      */
     public getCpf(): string {
         return this.cpf;
     }
 
     /**
-     * Define o cpf do cliente.
-     *
-     * @param cpf - O CPF do cliente.
+     * Define o CPF do cliente.
+     * 
+     * @param cpf - O CPF a ser definido para o cliente.
      */
     public setCpf(cpf: string): void {
         this.cpf = cpf;
     }
 
     /**
-     * Retorna o telefone do cliente.
+     * Retorna o número de telefone do cliente.
      *
-     * @returns {string} O telefone do cliente.
+     * @returns {string} O número de telefone do cliente.
      */
     public getTelefone(): string {
         return this.telefone;
     }
 
     /**
-     * Define o telefone do cliente.
-     * 
-     * @param telefone - O telefone a ser definido para o Cliente.
+     * Define o número de telefone do cliente.
+     *
+     * @param telefone - O número de telefone a ser definido.
      */
     public setTelefone(telefone: string): void {
         this.telefone = telefone;
+    }
+
+    /**
+     * Busca e retorna uma lista de clientes do banco de dados.
+     * @returns Um array de objetos do tipo `Cliente` em caso de sucesso ou `null` se ocorrer um erro durante a consulta.
+     * 
+     * - A função realiza uma consulta SQL para obter todos os registros da tabela "cliente".
+     * - Os dados retornados são utilizados para instanciar objetos da classe `Cliente`.
+     * - Cada cliente instanciado é adicionado a uma lista que será retornada ao final da execução.
+     * - Se houver uma falha na consulta ao banco, a função captura o erro, exibe uma mensagem no console e retorna `null`.
+     */
+    static async listagemClientes(): Promise<Array<Cliente> | null> {
+        const listaDeClientes: Array<Cliente> = [];
+
+        try {
+            const querySelectCliente = `SELECT * FROM cliente`;
+            const respostaBD = await database.query(querySelectCliente);
+
+            respostaBD.rows.forEach((linha) => {
+                const novoCliente = new Cliente(
+                    linha.nome,
+                    linha.cpf,
+                    linha.telefone
+                );
+
+                novoCliente.setIdCliente(linha.id_cliente);
+
+                listaDeClientes.push(novoCliente);
+            });
+            
+            return listaDeClientes;
+        } catch (error) {
+            console.log('Erro ao buscar lista de carros');
+            return null;
+        }
+    }
+
+    
+    /**
+     * Realiza o cadastro de um cliente no banco de dados.
+     * 
+     * Esta função recebe um objeto do tipo `Cliente` e insere seus dados (marca, modelo, ano e cor)
+     * na tabela `cliente` do banco de dados. O método retorna um valor booleano indicando se o cadastro 
+     * foi realizado com sucesso.
+     * 
+     * @param {Cliente} cliente - Objeto contendo os dados do cliente que será cadastrado. O objeto `Cliente`
+     *                        deve conter os métodos `getMarca()`, `getModelo()`, `getAno()` e `getCor()`
+     *                        que retornam os respectivos valores do cliente.
+     * @returns {Promise<boolean>} - Retorna `true` se o cliente foi cadastrado com sucesso e `false` caso contrário.
+     *                               Em caso de erro durante o processo, a função trata o erro e retorna `false`.
+     * 
+     * @throws {Error} - Se ocorrer algum erro durante a execução do cadastro, uma mensagem de erro é exibida
+     *                   no console junto com os detalhes do erro.
+     */
+    static async cadastroCliente(cliente: Cliente): Promise<boolean> {
+        try {
+            // query para fazer insert de um cliente no banco de dados
+            const queryInsertCliente = `INSERT INTO cliente (nome, cpf, telefone)
+                                        VALUES
+                                        ('${cliente.getNome()}', 
+                                        '${cliente.getCpf()}',  
+                                        '${cliente.getTelefone()}')
+                                        RETURNING id_cliente;`;
+
+            // executa a query no banco e armazena a resposta
+            const respostaBD = await database.query(queryInsertCliente);
+
+            // verifica se a quantidade de linhas modificadas é diferente de 0
+            if (respostaBD.rowCount != 0) {
+                console.log(`Cliente cadastrado com sucesso! ID do cliente: ${respostaBD.rows[0].id_cliente}`);
+                // true significa que o cadastro foi feito
+                return true;
+            }
+            // false significa que o cadastro NÃO foi feito.
+            return false;
+
+            // tratando o erro
+        } catch (error) {
+            // imprime outra mensagem junto com o erro
+            console.log('Erro ao cadastrar o cliente. Verifique os logs para mais detalhes.');
+            // imprime o erro no console
+            console.log(error);
+            // retorno um valor falso
+            return false;
+        }
     }
 }
--- src/model/PedidoVenda.ts
+++ src/model/PedidoVenda.ts
@@ -1,125 +1,207 @@
+import { DatabaseModel } from "./DatabaseModel";
+
+const database = new DatabaseModel().pool;
+
 /**
  * Classe que representa um Pedido de Venda.
  */
 export class PedidoVenda {
-
-    /* Atributos */
-    /* Identificador do pedido */
-    private idPedido = 0;
-    /* Identificador do Carro */
-    private idCarro;
-    /* Identificador do Cliente */
-    private idCliente;
-    /* Data do Pedido */
-    private data: Date;
-    /* Valor do Pedido */
-    private valor: number = 0;
+    /**
+     * Identificador único do pedido de venda.
+     */
+    private idPedido: number = 0;
+    /**
+     * Identificador do carro associado ao pedido de venda.
+     */
+    private idCarro: number;
+    /**
+     * Identificador do cliente associado ao pedido de venda.
+     */
+    private idCliente: number;
+    /**
+     * Data do pedido de venda.
+     */
+    private dataPedido: Date;
+    /**
+     * Valor total do pedido.
+     */
+    private valorPedido: number;
 
     /**
-     * Construtor da classe PedidoVenda
-     * 
-     * @param idPedido Identificador do pedido
-     * @param idCarro Identificador do Carro
-     * @param idCliente Identificador do cliente
-     * @param data Data de Realização do Pedido
-     * @param Valor Valor do Pedido
-*/
-    constructor(
-        idCarro: number,
-        idCliente: number,
-        data: Date,
-        valor: number,
-    ) {
+     * Construtor da classe PedidoVenda.
+     * @param idCarro - Identificador do carro.
+     * @param idCliente - Identificador do cliente.
+     * @param dataPedido - Data do pedido.
+     * @param valorPedido - Valor do pedido.
+     */
+    constructor(idCarro: number, idCliente: number, dataPedido: Date, valorPedido: number) {
         this.idCarro = idCarro;
         this.idCliente = idCliente;
-        this.data = data;
-        this.valor = valor;
+        this.dataPedido = dataPedido;
+        this.valorPedido = valorPedido;
     }
 
-    /* Métodos get e set */
     /**
-     * Recupera o identificador do Pedido
-     * @returns o identificador do Pedido
+     * Obtém o identificador do pedido.
+     * @returns O identificador do pedido.
      */
     public getIdPedido(): number {
         return this.idPedido;
     }
 
     /**
-     * Atribui um valor ao identificador do Pedido
-     * @param idCarro identificador do carro
+     * Define o identificador do pedido.
+     * @param idPedido - Novo identificador do pedido.
      */
     public setIdPedido(idPedido: number): void {
         this.idPedido = idPedido;
     }
 
-    /*
-    * Recupera o identificador do carro
-    * @returns  O Identificador do carro.
-    */
+    /**
+     * Obtém o identificador do carro.
+     * @returns O identificador do carro.
+     */
     public getIdCarro(): number {
         return this.idCarro;
     }
 
     /**
-     * Atribui um valor ao identificador do carro
-     * @param idCarro novo identificador do carro
+     * Define o identificador do carro.
+     * @param idCarro - Novo identificador do carro.
      */
     public setIdCarro(idCarro: number): void {
         this.idCarro = idCarro;
     }
 
-    /*
-       * Recupera o identificador do cliente
-       * @returns  O identificador do cliente
-       */
+    /**
+     * Obtém o identificador do cliente.
+     * @returns O identificador do cliente.
+     */
     public getIdCliente(): number {
         return this.idCliente;
     }
 
     /**
-     * Atribui um valor ao identificador do cliente
-     * @param idCliente novo identificador do cliente
+     * Define o identificador do cliente.
+     * @param idCliente - Novo identificador do cliente.
      */
     public setIdCliente(idCliente: number): void {
         this.idCliente = idCliente;
     }
 
-
     /**
-        * Define a data da Realização do Pedido.
-        * @param data - Data de Realização do Pedido.
-        */
-    public setData(data: Date): void {
-        this.data = data;
-    }
-
-
-    /**
-     * Retorna a data de Realização do Pedido.
-     *
-     * @returns A data de realização do Pedido.
+     * Obtém a data do pedido.
+     * @returns A data do pedido.
      */
-    public getData(): Date {
-        return this.data;
-    }
-
-
-    /**
-        * Define o Valor do Pedido.
-        *
-        * @param valor - Valor do Pedido.
-        */
-    public setValor(valor: number): void {
-        this.valor = valor;
+    public getDataPedido(): Date {
+        return this.dataPedido;
     }
 
     /**
-     * Retorna o valor do pedido.
-     *
+     * Define a data do pedido.
+     * @param dataPedido - Nova data do pedido.
+     */
+    public setDataPedido(dataPedido: Date): void {
+        this.dataPedido = dataPedido;
+    }
+
+    /**
+     * Obtém o valor do pedido.
      * @returns O valor do pedido.
      */
-    public getValor(): number {
-        return this.valor;
+    public getValorPedido(): number {
+        return this.valorPedido;
+    }
+
+    /**
+     * Define o valor do pedido.
+     * @param valorPedido - Novo valor do pedido.
+     */
+    public setValorPedido(valorPedido: number): void {
+        this.valorPedido = valorPedido;
+    }
+
+    /**
+     * Busca e retorna uma lista de pedidos de venda do banco de dados.
+     * @returns Um array de objetos do tipo `PedidoVenda` em caso de sucesso ou `null` se ocorrer um erro durante a consulta.
+     * 
+     * - A função realiza uma consulta SQL para obter todos os registros da tabela "pedido_venda".
+     * - Os dados retornados são utilizados para instanciar objetos da classe `PedidoVenda`.
+     * - Cada pedido de venda instanciado é adicionado a uma lista que será retornada ao final da execução.
+     * - Caso ocorra uma falha na consulta ao banco, a função captura o erro, exibe uma mensagem no console e retorna `null`.
+     */
+    static async listagemPedidos(): Promise<Array<PedidoVenda> | null> {
+        const listaDePedidos: Array<PedidoVenda> = [];
+
+        try {
+            const querySelectPedidos = `SELECT * FROM pedido_venda;`;
+            const respostaBD = await database.query(querySelectPedidos);
+
+            respostaBD.rows.forEach((linha) => {
+                const novoPedidoVenda = new PedidoVenda(
+                    linha.id_carro,
+                    linha.id_cliente,
+                    linha.data_pedido,
+                    parseFloat(linha.valor_pedido)
+                );
+
+                novoPedidoVenda.setIdPedido(linha.id_pedido);
+
+                listaDePedidos.push(novoPedidoVenda);
+            });
+
+            return listaDePedidos;
+        } catch (error) {
+            console.log('Erro ao buscar lista de pedidos');
+            return null;
+        }
+    }
+    
+    /**
+     * Realiza o cadastro de um carro no banco de dados.
+     * 
+     * Esta função recebe um objeto do tipo `Carro` e insere seus dados (marca, modelo, ano e cor)
+     * na tabela `carro` do banco de dados. O método retorna um valor booleano indicando se o cadastro 
+     * foi realizado com sucesso.
+     * 
+     * @param {PedidoVenda} pedidoVenda - Objeto contendo os dados do carro que será cadastrado. O objeto `Carro`
+     *                        deve conter os métodos `getMarca()`, `getModelo()`, `getAno()` e `getCor()`
+     *                        que retornam os respectivos valores do carro.
+     * @returns {Promise<boolean>} - Retorna `true` se o carro foi cadastrado com sucesso e `false` caso contrário.
+     *                               Em caso de erro durante o processo, a função trata o erro e retorna `false`.
+     * 
+     * @throws {Error} - Se ocorrer algum erro durante a execução do cadastro, uma mensagem de erro é exibida
+     *                   no console junto com os detalhes do erro.
+     */
+    static async cadastroPedido(pedidoVenda: PedidoVenda): Promise<boolean> {
+        try {
+            // query para fazer insert de um carro no banco de dados
+            const queryInsertPedido_venda = `INSERT INTO pedidoVenda (DataPedido, ValorPedido)
+                                        VALUES
+                                        ('${pedidoVenda.getDataPedido()}', 
+                                        '${pedidoVenda.getValorPedido()}',
+                                        RETURNING id_pedido;`;
+
+            // executa a query no banco e armazena a resposta
+            const respostaBD = await database.query(queryInsertPedido_venda);
+
+            // verifica se a quantidade de linhas modificadas é diferente de 0
+            if (respostaBD.rowCount != 0) {
+                console.log(`Carro cadastrado com sucesso! ID do pedido: ${respostaBD.rows[0].id_pedido}`);
+                // true significa que o cadastro foi feito
+                return true;
+            }
+            // false significa que o cadastro NÃO foi feito.
+            return false;
+
+            // tratando o erro
+        } catch (error) {
+            // imprime outra mensagem junto com o erro
+            console.log('Erro ao cadastrar o pedido. Verifique os logs para mais detalhes.');
+            // imprime o erro no console
+            console.log(error);
+            // retorno um valor falso
+            return false;
+        }
     }
 }
